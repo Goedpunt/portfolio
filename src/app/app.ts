abe453b42a14f112f7b1c3c36e658412d93c70aa
Dani <danielremibeau@gmail.com>
2026-04-04 11:26:19 +0200

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './header/header';
+import { Hero } from './hero/hero';
+import { About } from './about/about';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header, Hero, About, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('portfolio-2');
-}
+export class AppComponent {}
